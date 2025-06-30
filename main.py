f56155d9273a08400ed38dfe343044b110007e06
BitByBitRN <bitbybitrn@gmail.com>
2025-06-30 19:44:26 -0400

    refactored code

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-def get_book_text(file_path):
-    with open(file_path) as f:
-        return f.read()
-    
 def main():
     text = get_book_text("books/frankenstein.txt")
     num_words = count_words(text)
     print(f"{num_words} words found in the document")
 
-def count_words(text):
-    return len(text.split())
+def get_book_text(file_path):
+    with open(file_path) as f:
+        return f.read()
+    
+from stats import count_words
 
 main()
