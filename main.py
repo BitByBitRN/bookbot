78e79248c814d5934a7500725859f038ab685bce
BitByBitRN <bitbybitrn@gmail.com>
2025-06-30 21:08:21 -0400

    created function to count occurrences of each character in text

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 def main():
+    from stats import count_words
+    from stats import count_char
+
     text = get_book_text("books/frankenstein.txt")
     num_words = count_words(text)
     print(f"{num_words} words found in the document")
+    num_char = count_char(text)
+    print(num_char)
 
 def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
     
-from stats import count_words
-
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,13 @@
 def count_words(text):
     return len(text.split())
+
+def count_char(text):
+    text_lower = text.lower()
+    char_dict = {}
+
+    for char in text_lower:
+        if char not in char_dict:
+            char_dict[char] = 0
+        char_dict[char] += 1
+
+    return char_dict
